beec472d162a15c1b49c65cbfd03d57d9914ad7a
Bhim Debnath <bhimdebnath70@gmail.com>
2025-08-12 10:26:00 +0530

    Add files via upload

---
+++ styled-card.components.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-styled-card',
+  standalone: true, // ✅ Required
+  templateUrl: './styled-card.component.html',
+  styleUrls: ['./styled-card.component.css']
+})
+export class StyledCardComponent {}
